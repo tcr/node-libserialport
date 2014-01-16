1c8101eb07b35073b60106e693d8da523ce9a33e
Tim Cameron Ryan <id@timryan.org>
2014-01-16 14:27:01 -0500

    Filter filter

--- xsp/linux.js
+++ xsp/linux.js
@@ -2,6 +2,6 @@ var fs = require('fs');
 var _path = require('path');
 var exec = require('child_process').exec;
 
-exec('find /sys/devices | grep usb | grep tty', function (err, stdout, stderr) {
+exec('find /sys/devices | grep usb | grep \"tty\\w\\+$\"', function (err, stdout, stderr) {
 	console.log(stdout);
 });
