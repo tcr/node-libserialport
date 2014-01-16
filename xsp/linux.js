a3ca362a3a3f1c12bd396750089d9b57a936050d
Tim Cameron Ryan <id@timryan.org>
2014-01-16 14:21:47 -0500

    exec hardcore

--- xsp/linux.js
+++ xsp/linux.js
@@ -1,63 +1,7 @@
 var fs = require('fs');
 var _path = require('path');
+var exec = require('child_process').exec;
 
-function readdirRecursive (baseDir, fn) {
-    baseDir = baseDir.replace(/\/$/, '');
-
-    var waitCount = 0;
-
-    function readdirRecursive(curDir) {
-        var prependcurDir = function(fname){
-            return _path.join(curDir, fname);
-        };
-
-        waitCount++;
-        fs.readdir(curDir, function(e, curFiles) {
-            if (e) {
-                fn(e);
-                return;
-            }
-            waitCount--;
-
-            curFiles = curFiles.map(prependcurDir);
-
-            curFiles.forEach(function(it) {
-                waitCount++;
-
-                fs.stat(it, function(e, stat) {
-                    waitCount--;
-
-                    if (e) {
-                        fn(e);
-                    } else {
-                        if (stat.isDirectory()) {
-                            readdirRecursive(it);
-                        }
-                    }
-
-                    if (waitCount == 0) {
-                        fn(null, null);
-                    }
-                });
-            });
-
-            fn(null, curFiles.map(function(val) {
-                // convert absolute paths to relative
-                return _path.relative(baseDir, val);
-            }));
-
-            if (waitCount == 0) {
-                fn(null, null);
-            }
-        });
-    };
-
-    readdirRecursive(baseDir);
-}
-
-readdirRecursive('/sys/devices', function (err, path) {
-	console.log(path);
-	if (path === null) {
-		console.log('done');
-	}
-})
+exec('find /sys/devices | grep tty', function (err, stdout, stderr) {
+	console.log(stdout);
+});
