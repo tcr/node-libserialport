e4849959a087147e13d522cfb32b043c9f11fa2c
Tim Cameron Ryan <id@timryan.org>
2014-01-16 14:28:05 -0500

    Path

--- xsp/linux.js
+++ xsp/linux.js
@@ -3,5 +3,9 @@ var _path = require('path');
 var exec = require('child_process').exec;
 
 exec('find /sys/devices | grep usb | grep \"tty\\w\\+$\"', function (err, stdout, stderr) {
-	console.log(stdout);
+	stdout.split(/\r?\n/).filter(function (a) {
+		return (a);
+	}).forEach(function (path) {
+		console.log(path.replace(/[^\/]+:[^:]+$/, ''))
+	})
 });
