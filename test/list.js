4745d65764b94ab7dcd47fa3fbe5f50e37a54d89
Tim Cameron Ryan <id@timryan.org>
2014-01-20 12:30:01 -0500

    Adds a real-world npm test

--- test/list.js
+++ test/list.js
@@ -1,5 +1,13 @@
-var libserialport = require('..');
+var test = require('tape')
 
-libserialport.list(function (err, modems) {
-	console.log('Modems:', modems);
-})
+test('list test', function (t) {
+	t.plan(2);
+
+	var libserialport = require('../');
+
+	t.doesNotThrow(function () {
+		libserialport.list(function (err, modems) {
+			t.ok(typeof modems.length == 'number');
+		})
+	});
+});
